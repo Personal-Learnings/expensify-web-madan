44686f9ee18d34de78b6f7f20e1d64d17bbd7df6
EXI-VENKATESAN <madanraj.venkatesan@boeing.com>
2018-12-03 12:25:45 -0800

    After Initial Project Setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = (
+  <div>
+    <p>Hello Its Working</p>
+  </div>
+);
+
+ReactDOM.render(template, document.getElementById("app"));
